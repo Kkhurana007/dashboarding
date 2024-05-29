6e21c50580ee975218548eaf31c8cc883e91d98a
Kunal Khurana <91566244+Kkhurana007@users.noreply.github.com>
2024-05-29 17:33:23 -0400

    Add files via upload

---
+++ dashboard.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import plotly.express as px
+import pandas as pd
+import os
+import warnings
+warnings.filterwarnings('ignore')
+
+
+st.set_page_config(page_title="Superstore!!!", page_icon=":bar_chart", layout="wide")
+
+st.title(" :bar_chart: Superstore.csv")
+st.markdown('<style>div.block-container{padding-top:2rem;}</style>',unsafe_allow_html=True)
+
+
+fl = st.file_uploader(":file_folder: Upload a file",type=(["csv","txt","xlsx","xls"]))
+
+if fl is not None:
+    filename = fl.name
+    st.write(filename)
+    df = pd.read_csv(filename)
+else:
+    os.chdir(r"E:\dashboarding")
+    df = pd.read_csv("Sample - Superstore.csv", encoding = "utf_16_be")
+
+col1, col2 = st.columns((2))
+df["Order Date"] = pd.to_datetime(df["Order Date"])
+
+# Getting the min and max date 
+startDate = pd.to_datetime(df["Order Date"]).min()
+endDate = pd.to_datetime(df["Order Date"]).max()
+
+with col1:
+    date1 = pd.to_datetime(st.date_input("Start Date", startDate))
+with col2:
+    date2 = pd.to_datetime(st.date_input("End Date", endDate))
+    
+df = df[(df["Order Date"] >= date1) & (df["Order Date"] <= date2)].copy()
+
+# create a side filter based on group and series_title
+
+st.sidebar.header("Choose your filter: ")
